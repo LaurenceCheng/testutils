62db0b7aa3081076ed4903dd62739d1aefe8da3a
Laurence Cheng <laurence10+github@gmail.com>
2019-08-19 02:39:48 +0800

    Make test cases failed for snapshot

--- use/main_test.go
+++ use/main_test.go
@@ -48,7 +48,7 @@ func TestMethodP2(t *testing.T) {
 			TargetType: tu.GetType(lib.MyStruct{}),
 			TestMethod: "MethodP2",
 			Args:       tu.GetArgs(3, 4),
-			Want:       tu.GetWant(12),
+			Want:       tu.GetWant(15),
 		},
 	}
 
@@ -62,7 +62,7 @@ func TestMethodP2R2(t *testing.T) {
 			TargetType: tu.GetType(lib.MyStruct{}),
 			TestMethod: "MethodP2R2",
 			Args:       tu.GetArgs(3, 4),
-			Want:       tu.GetWant(12, "12"),
+			Want:       tu.GetWant(12, 12),
 		},
 	}
 
